f128ea1019ec43bb9dba11b45969109902bb0292
jianghe <573748150@163.com>
2018-04-02 15:50:19 +0800

    新增es6字符串文档

--- code/rollup/build/bundle3.js
+++ code/rollup/build/bundle3.js
@@ -29,5 +29,102 @@
     u1.fun1('u1.fun1');
     fn1();
     fn2();
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
 
 })));
