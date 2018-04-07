7e621b775a7c6775394c63b9d730eab5d682b2b2
jianghe <573748150jh@163.com>
2018-04-07 20:55:42 +0800

    提交

--- code/rollup/build/bundle.js
+++ code/rollup/build/bundle.js
@@ -7,7 +7,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -42,7 +42,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -77,7 +77,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -112,7 +112,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -147,7 +147,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -182,7 +182,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -217,7 +217,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -252,7 +252,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -287,7 +287,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -322,7 +322,7 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
@@ -355,7 +355,388 @@
 
     var u1 = {
         fun1: function fun1(test) {
-            var a = {test: '123', test1: '345'};
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
             for (var _item in a) {
                 console.log(_item);
             }
--- code/rollup/src/util1.js
+++ code/rollup/src/util1.js
@@ -1,10 +1,13 @@
 export default {
     fun1: function (test) {
-        var a = {test: '123', test1: '345'};
+        var a = { test: '123', test1: '345' };
         for (let item in a) {
             console.log(item);
         }
         console.log(item);//可以读出item
         console.log(sss);
+    },
+    fun: function (mess) {
+        console.log(mess);
     }
 }
