bdc33bb02f4568aa8e0ef071fc5f522ba1a5cb6d
jianghe <573748150jh@163.com>
2018-04-09 18:28:40 +0800

    update

--- code/rollup/src/util2.js
+++ code/rollup/src/util2.js
@@ -8,3 +8,73 @@ export function fn2() {
     console.log('fn2');
 };
 
+export function fn1() {
+    var ss = [1, 2, 3, 4, 5]
+    ss = ss.map(item => item + 1);
+    console.log(ss.join(','));
+};
+
+export function fn2() {
+    console.log('fn2');
+};
+
+export function fn1() {
+    var ss = [1, 2, 3, 4, 5]
+    ss = ss.map(item => item + 1);
+    console.log(ss.join(','));
+};
+
+export function fn2() {
+    console.log('fn2');
+};
+
+export function fn1() {
+    var ss = [1, 2, 3, 4, 5]
+    ss = ss.map(item => item + 1);
+    console.log(ss.join(','));
+};
+
+export function fn2() {
+    console.log('fn2');
+};
+
+export function fn1() {
+    var ss = [1, 2, 3, 4, 5]
+    ss = ss.map(item => item + 1);
+    console.log(ss.join(','));
+};
+
+export function fn2() {
+    console.log('fn2');
+};
+
+export function fn1() {
+    var ss = [1, 2, 3, 4, 5]
+    ss = ss.map(item => item + 1);
+    console.log(ss.join(','));
+};
+
+export function fn2() {
+    console.log('fn2');
+};
+
+export function fn1() {
+    var ss = [1, 2, 3, 4, 5]
+    ss = ss.map(item => item + 1);
+    console.log(ss.join(','));
+};
+
+export function fn2() {
+    console.log('fn2');
+};
+
+export function fn1() {
+    var ss = [1, 2, 3, 4, 5]
+    ss = ss.map(item => item + 1);
+    console.log(ss.join(','));
+};
+
+export function fn2() {
+    console.log('fn2');
+};
+
