52c5d7d963e0c456524da87323e26d6b55e76bb7
jianghe <573748150jh@163.com>
2018-04-16 18:23:45 +0800

    update

--- code/rollup/build/bundle.js
+++ code/rollup/build/bundle.js
@@ -68,3 +68,73 @@
     fn2();
 
 })));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
+
+    var u1 = {
+        fun1: function fun1(test) {
+            var a = { test: '123', test1: '345' };
+            for (var _item in a) {
+                console.log(_item);
+            }
+            console.log(item); //可以读出item
+            console.log(sss);
+        }
+    };
+
+    function fn1() {
+        var ss = [1, 2, 3, 4, 5];
+        ss = ss.map(function (item) {
+            return item + 1;
+        });
+        console.log(ss.join(','));
+    }
+
+    function fn2() {
+        console.log('fn2');
+    }
+
+    u1.fun1('u1.fun1');
+    fn1();
+    fn2();
+
+})));
